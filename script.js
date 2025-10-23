c0e040b7228079532f7e3ff661c24729d7316e5c
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-10-23 12:17:00 +0000

    começado

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+    //document.gatElementeById("lampada").src="on.png"
+    console.log("on")
+}
+
+function apagar(){
+    //document.gatElementeById("lampada").src="off.png"
+    console.log("off")
+}
